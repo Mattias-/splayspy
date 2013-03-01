e97e570c98e74f9301f7752c7c92e378ada08fbc
Mattias- <mattias@ppelgren.se>
2013-03-01 06:20:08 +0000

    Cleaning up

--- core.py
+++ core.py
@@ -48,38 +48,6 @@ class Channel(object):
         print 'source_prog_list', len(source_prog_list)
         return source_prog_list
 
-    def diffPrograms2(self, source_prog_list):
-        db_list = []
-        # get programs from db
-        filename = 'db/%s.txt' % self.name
-        try:
-            with open(filename, 'r') as infile:
-                db_list = json.load(infile)
-        except IOError as e:
-            print 'no file %s' % filename
-            os.makedirs('db/%s_programs' % self.name)
-            print 'created program dir'
-
-        (new, old, current) = utils.diffDicts(source_prog_list, db_list,
-                                              utils.progHash)
-        if new: print 'added', new
-
-        now = datetime.datetime.utcnow().isoformat()
-        for d in new:
-            d['first_seen'] = d['last_seen'] = now
-            d['seen_counter'] = 1
-        for d in current:
-            d['last_seen'] = now
-            d['seen_counter'] = d.get('seen_counter', 1) + 1
-
-        db_list = new+old+current
-
-        with open(filename, 'w') as outfile:
-            json.dump(db_list, outfile)
-
-        print 'source_prog_list', len(source_prog_list)
-        return source_prog_list
-
     def updatePrograms(self):
         d = gethttp.getPageData(self.all_programs_url)
         d.addCallback(self.getSourcePrograms)
@@ -119,7 +87,6 @@ class Channel(object):
         #else:
         #    raise Exception('too many results')
 
-        #(new, old, current) = utils.diffDicts(episodes, db_list,
         (old, new, current) = utils.diffDicts(db_list, episodes,
                                               utils.episodeHash)
         now = datetime.datetime.utcnow().isoformat()
@@ -133,34 +100,6 @@ class Channel(object):
         if new: print 'added', new
 
         db_program.update({'episodes': new+old+current}).run()
-
-    def diffEpisodes2(self, episodes, program):
-        print program['channel'], program['name'], len(episodes)
-        db_list = []
-
-        # get programs from db
-        filename = 'db/%s_programs/%s.txt' % (program['channel'], program['id'])
-        try:
-            with open(filename, 'r') as infile:
-                db_list = json.load(infile)
-        except IOError as e:
-            print 'no file %s' % filename
-
-        (new, old, current) = utils.diffDicts(episodes, db_list,
-                                              utils.episodeHash)
-        now = datetime.datetime.utcnow().isoformat()
-        for d in new:
-            d['first_seen'] = d['last_seen'] = now
-            d['seen_counter'] = 1
-        for d in current:
-            d['last_seen'] = now
-            d['seen_counter'] = d.get('seen_counter', 1) + 1
-
-        if new: print 'added', new
-        db_list = new+old+current
-
-        with open(filename, 'w') as outfile:
-            json.dump(db_list, outfile)
 
     def getProgramEpisodes(self, program):
         url = self.episodes_url % str(program['id'])

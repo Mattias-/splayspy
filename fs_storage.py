4c4223641cdd943daa73a39285de4a45525b1d9d
Mattias- <mattias@ppelgren.se>
2013-10-26 01:12:20 +0200

    Back to file storage, fixed updatePrograms, bug in diff :(

---
+++ fs_storage.py
@@ -0,0 +1,40 @@
+
+import json
+import os
+
+
+def get_programs(channel):
+    db_list = []
+    filename = 'db/%s.txt' % channel.name
+    try:
+        with open(filename, 'r') as infile:
+            db_list = json.load(infile)
+    except IOError as e:
+        print 'no file %s' % filename
+    return db_list
+
+
+def save_programs(channel, prog_list):
+    filename = 'db/%s.txt' % channel.name
+    with open(filename, 'w') as outfile:
+        json.dump(prog_list, outfile)
+
+
+def get_episodes(channel, program):
+    db_list = []
+    dirname = 'db/%s_programs' % program['channel']
+    filename = '%s/%s.txt' % (dirname, program['id'])
+    try:
+        with open(filename, 'r') as infile:
+            db_list = json.load(infile)
+    except IOError as e:
+        print 'no file %s' % filename
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+    return db_list
+
+
+def save_episodes(channel, program, episode_list):
+    filename = 'db/%s_programs/%s.txt' % (program['channel'], program['id'])
+    with open(filename, 'w') as outfile:
+        json.dump(episode_list, outfile)
--- splays.py
+++ splays.py
@@ -5,28 +5,30 @@ import tv4play
 
 log = logging.getLogger("splays")
 
+
 def main():
     log.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s::%(name)s::%(levelname)s::%(message)s')
+    fmt = '%(asctime)s::%(name)s::%(levelname)s::%(message)s'
+    formatter = logging.Formatter(fmt)
+
     fh = logging.FileHandler("splays.log", mode='w')
     fh.setFormatter(formatter)
+    log.addHandler(fh)
+
     sh = logging.StreamHandler()
     sh.setFormatter(formatter)
-    log.addHandler(fh)
     log.addHandler(sh)
 
     log.info("Started splays scraper & parser")
 
-    svt = svtplay.SVTplay()
-    tv4 = tv4play.TV4play()
-    channels = [\
-                svt,
-  #              tv4,
-                ]
+    channels = [
+        svtplay.SVTplay(),
+        #tv4play.TV4play(),
+    ]
     log.info("Channels: %s" % channels)
-
     for c in channels:
-        pl = c.updatePrograms()
-        c.updateProgramEpisodes(pl)
+        pl = c.getPrograms()
+        #c.updateProgramEpisodes(pl)
 
-main()
+if __name__ == '__main__':
+    main()
